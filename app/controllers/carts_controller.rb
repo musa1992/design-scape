b6cc6c948b0dfd45baa77ae51f278208392bb66d
musa1992 <mosesbwire91@gmail.com>
2021-07-06 23:00:04 +0300

    add checkout page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,19 +1,21 @@
 class CartsController < ApplicationController
 
     def show
-        @order = Order.find(session[:order_id])  
+        @order = Order.find(5)  
     end
 
     def add_to_cart
+        
         if session.key?(:order_id)
             order = Order.find(session[:order_id])
             order.ordered_items.build(product_id: params[:id]).save
-        
         else
+            
+            
             account = current_user.account
-            @order = account.orders.build()
+            @order = account.orders.build(pick_up_station_id: 1)
             if @order.save
-                session[:order_id] = @order.id
+                session[:order_id]
                 @order.ordered_items.build(product_id: params[:id]).save
             end
             
--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,5 @@
 class CheckoutController < ApplicationController
+
+    def index
+    end
 end
