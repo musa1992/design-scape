b1fdcebb664d93cbb622564dca447138b741126b
musa1992 <mosesbwire91@gmail.com>
2021-07-22 22:13:02 +0300

    add address and delivery partials , add checkout.js add form to address partial

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,7 @@
 class CheckoutController < ApplicationController
 
     def index
+        @address = Address.new
+        @cities = City.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :products
   resources :merchant_accounts
   resources :checkout, only: [:index]
+  resources :addresses
   resource :cart, except: [:create]
 end
