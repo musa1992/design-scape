4fa48d9f06a3b30540e7dc07599167e084478acb
musa1992 <mosesbwire91@gmail.com>
2021-08-02 21:13:55 +0300

    add select pick up station modal dropdown select for station city and estate

--- app/javascript/custom/checkout.js
+++ app/javascript/custom/checkout.js
@@ -2,29 +2,32 @@ document.addEventListener("turbolinks:load", () => {
     if (!Turbolinks) {
         location.reload();
         }
-    var estate_select = document.getElementById("estate")
+    var estateSelect = document.getElementById("estate")
+    var deliveryEstate = document.getElementById("delivery-estate")
+
     var city = document.getElementById("address_city")
+    var deliveryCity = document.getElementById("address_city_id")
     var modal = document.getElementById("myModal")
     var triggerModal = document.getElementById("trigger-modal") 
     var span = document.getElementsByClassName("close")[0]
 
-    function getEstate(town){
+    function getEstate(town,estate){
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
                 var est = JSON.parse(this.responseText).estates
                 for (let i = 0; i < est.length; i++){
                     var option = '<option>' + est[i] + '</option>'
-                    estate_select.insertAdjacentHTML('beforeend', option);
+                    estate.insertAdjacentHTML('beforeend', option);
                 }   
             }
         };
         xhttp.open("GET", "/get_estate?city_name="+ town, true)
         xhttp.send();
     }  
-    function setEstateOnLoad(){
+    function setEstateOnLoad(city,estate){
             var selected_city = city.options[city.selectedIndex].text
-            getEstate(selected_city)   
+            getEstate(selected_city,estate)   
     }
     
     function removeChildNodes(parent){
@@ -34,19 +37,37 @@ document.addEventListener("turbolinks:load", () => {
     }
 
     if (city){
-        setEstateOnLoad();
+        setEstateOnLoad(city,estateSelect);
     }
+
+    
+
     if (city){
         city.addEventListener('click', function(e){
             var chosen_city = this.options[this.selectedIndex].text
-            removeChildNodes(estate_select)
-            getEstate(chosen_city);
+            removeChildNodes(estateSelect)
+            getEstate(chosen_city,estateSelect);
             
         })
     }
 
+    if (deliveryCity){
+        setEstateOnLoad(deliveryCity,deliveryEstate)
+    } 
+
+    if (deliveryCity){
+        deliveryCity.addEventListener('click', function(e){
+            var chosen_city = this.options[this.selectedIndex].text
+            removeChildNodes(deliveryEstate)
+            getEstate(chosen_city,deliveryEstate);
+            
+        })
+    }
+
+
+
     triggerModal.onclick = function(){
-        modal.style.display = "block";
+        modal.style.display = "block";  
     }
 
     span.onclick = function(){
